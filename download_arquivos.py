d9df33c118f52317357a0b7743d9d7415d2310b4
carlos.arruda <carlos.arruda@ndd.tech>
2024-05-07 16:07:43 -0300

    Ajuste para armazenar o tamanho do arquivo no momento do download no arquivo links.csv

--- download_arquivos.py
+++ download_arquivos.py
@@ -13,15 +13,19 @@ def download_file(url, filename, folder):
             os.makedirs('downloads')
 
         # Verifica se a pasta do ambiente existe e cria se não existir
-        folder_path = os.path.join('downloads', folder)
+        folder_path = str(os.path.join('downloads', folder))
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
         # Extrai a extensão do nome do arquivo na URL
-        filename_extension = os.path.splitext(urlparse(url).path)[-1]
+        filename_extension = str(os.path.splitext(urlparse(url).path)[-1])
+        filename = str(filename)
 
-        # Junta o diretório 'downloads' com o nome do arquivo e sua extensão
-        filepath = os.path.join(folder_path, filename + filename_extension)
+        try:
+            # Junta o diretório 'downloads' com o nome do arquivo e sua extensão
+            filepath = os.path.join(folder_path, filename + filename_extension)
+        except Exception as e:
+            print(f"Erro ao montar o caminho do arquivo: {e}")
 
         # Verifica se o arquivo já existe na pasta 'downloads'
         if os.path.exists(filepath):
@@ -42,19 +46,17 @@ def download_file(url, filename, folder):
 
         progress_bar.close()
 
-        # Retorna o tamanho do arquivo baixado
-        return 'OK', os.path.getsize(filepath)
-
         if total_size != 0 and progress_bar.n != total_size:
             print("Erro ao fazer o download completo do arquivo.")
             return 'Erro'
         else:
             print(f"Download concluído: {filepath}")
-            return 'OK'
+            # Retorna o tamanho do arquivo baixado
+            return 'OK', os.path.getsize(filepath)
 
     except Exception as e:
         print(f"Erro ao fazer o download de {url}: {e}")
-        return 'Erro'
+        return 'Erro', 0
 
 def create_report(downloads):
     # Cria um novo arquivo Excel e adiciona uma planilha
@@ -79,16 +81,21 @@ def main():
     # Abrindo o arquivo CSV e lendo os links, nomes de arquivo e ambientes
     df = pd.read_csv(csv_file, delimiter=';')
 
-    # Atualiza o arquivo links.csv com o tamanho do arquivo
-    for index, row in df.iterrows():
-        url = row['URL']  # Link de download
-        filename = row['Nome do Arquivo']  # Nome do arquivo
-        folder = row['Ambiente']  # Pasta onde o arquivo deve ser salvo
-        status, size = download_file(url, filename, folder)
-        df.at[index, 'Tamanho'] = size if status == 'OK' else 0
+    # Verifica se as colunas estão presentes no DataFrame
+    if 'url' in df.columns and 'nome_do_arquivo' in df.columns and 'ambiente' in df.columns:
+        # Atualiza o arquivo links.csv com o tamanho do arquivo
+        for index, row in df.iterrows():
+            url = row['url']  # Link de download
+            filename = row['nome_do_arquivo']  # Nome do arquivo
+            folder = row['ambiente']  # Pasta onde o arquivo deve ser salvo
+            status, size = download_file(url, filename, folder)
+            df.at[index, 'tamanho_origem'] = int(size) if status == 'OK' else 0
+            df.at[index, 'status'] = status
 
-    # Salva o arquivo CSV atualizado
-    df.to_csv(csv_file, sep=';', index=False)
+        # Salva o arquivo CSV atualizado
+        df.to_csv(csv_file, sep=';', index=False)  # Use ';' como separador ao salvar o arquivo
+    else:
+        print("As colunas 'url', 'nome_do_arquivo' e 'ambiente' não foram encontradas no arquivo CSV.")
 
 if __name__ == "__main__":
     main()
